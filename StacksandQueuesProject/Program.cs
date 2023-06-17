e33dbbd00bb3279efa7673772a2fb45372832a50
Poojachakor <chakorpooja07@gmail.com>
2023-06-17 09:35:27 +0530

    Add: StacksandQueuesProject

---
+++ StacksandQueuesProject/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace StacksandQueuesProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StackQueueDemo stackLinkedList = new StackQueueDemo();
+            stackLinkedList.Push(70);
+            stackLinkedList.Push(30);
+            stackLinkedList.Push(56);
+            stackLinkedList.Display();
+        }
+    }
+}
